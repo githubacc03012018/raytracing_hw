e8c866c4f6dafaab92624bd08eb85bb4b0845333
Martin <overflowingintelligence@gmail>
2022-03-02 15:47:06 +0200

    Lecture 2 homework

--- L2_hw/main.cpp
+++ L2_hw/main.cpp
@@ -1,8 +1,9 @@
-#include <format>
 #include <iostream>
 #include <fstream>
 #include "vector3.h"
-#include <cmath>
+#include <cmath>     
+#include <stdlib.h> 
+#include <random>
 
 using namespace std;
 
@@ -11,41 +12,128 @@ void WriteColor(fstream& file, Color pixel_color) {
 	file << static_cast<int>(255.999 * pixel_color.x()) << ' '
 		<< static_cast<int>(255.999 * pixel_color.y()) << ' '
 		<< static_cast<int>(255.999 * pixel_color.z()) << '\n';
-	 
+
 }
 
-int main() {
-	int width = 256;
-	int height = 256;
+inline double random_double(double min, double max) {
+	static uniform_real_distribution<double> distribution(min, max);
+	static mt19937 generator;
+	return distribution(generator);
+}
 
+void GenerateFirstImage(int width, int height) {
 	fstream myfile;
 	myfile.open("image1.ppm", ios::out);
-	 
+
 	myfile << "P3 \n";
 	myfile << width << " " << height << "\n";
 	myfile << "255 \n";
 
 	Color c;
-	auto r = (double(1) / (2));
-	//auto r = 1;
+	auto r = (double(1) / (4));
+
 	for (int i = height; i > 0; i--) {
 		for (int j = 0; j < width; j++) {
-			auto x = (double(j - 128) / (width));
-			auto y = (double(i - 128) / (height));
-			 
-			if (pow(x, 2) + pow(y, 2) < pow(r, 2) ) {
-				c = Color(0.2, 0.5, 0.25);
+			auto x = (double(j - width / 2) / (width));
+			auto y = (double(i - height / 2) / (height));
+
+			if (pow(x, 2) + pow(y, 2) < pow(r, 2)) {
+				c = Color(0.25, 0.25, random_double(0.5, 1.0));
 			}
 			else {
-				c = Color(0.1, 0.1, 0.25);
+				c = Color(0.25, 0.75, 0.25);
 			}
-			
+
 			WriteColor(myfile, c);
 		}
 	}
 
 
-	myfile.close(); 
+	myfile.close();
+}
+
+void GenerateSecondImage(int width, int height) {
+	fstream myfile;
+	myfile.open("image2.ppm", ios::out);
+
+	myfile << "P3 \n";
+	myfile << width << " " << height << "\n";
+	myfile << "255 \n";
+
+	for (int i = height; i > 0; i--) {
+		for (int j = 0; j < width; j++) {
+			Color c;
+			if (i >= 576 && i <= 768) {
+				if (j >= 0 && j <= 255) {
+					c = random_double(0.0, 1.0) >= 0.5 ? Color(random_double(0.0, 1.0), 0.0, 0.0) : Color(0.0, 0.0, 0.0);
+				}
+				if (j >= 256 && j <= 512) {
+					c = random_double(0.0, 1.0) >= 0.5 ? Color(0.0, random_double(0.0, 1.0), 0.0) : Color(0.0, 0.0, 0.0);
+				}
+				if (j >= 512 && j <= 768) {
+					c = random_double(0.0, 1.0) >= 0.5 ? Color(0.0, 0.0, random_double(0.0, 1.0)) : Color(0.0, 0.0, 0.0);
+				}
+				if (j >= 768 && j <= 1024) {
+					c = random_double(0.0, 1.0) >= 0.5 ? Color(random_double(0.0, 1.0), random_double(0.0, 1.0), random_double(0.0, 1.0)) : Color(0.0, 0.0, 0.0);
+				}
+			}
+
+			if (i >= 384 && i <= 576) {
+				if (j >= 0 && j <= 255) {
+					c = random_double(0.0, 1.0) >= 0.5 ? Color(0.0, random_double(0.0, 1.0), 0.0) : Color(0.0, 0.0, 0.0);
+				}
+				if (j >= 256 && j <= 512) {
+					c = random_double(0.0, 1.0) >= 0.5 ? Color(random_double(0.0, 1.0), 0.0, 0.0) : Color(0.0, 0.0, 0.0);
+				}
+				if (j >= 512 && j <= 768) {
+					c = random_double(0.0, 1.0) >= 0.5 ? Color(random_double(0.0, 1.0), random_double(0.0, 1.0), 1.0) : Color(0.0, 0.0, 0.0);
+				}
+				if (j >= 768 && j <= 1024) {
+					c = random_double(0.0, 1.0) >= 0.5 ? Color(1.0, random_double(0.0, 1.0), 0.0) : Color(0.0, 0.0, 0.0);
+				}
+			}
+
+			if (i >= 192 && i <= 384) {
+				if (j >= 0 && j <= 255) {
+					c = random_double(0.0, 1.0) >= 0.5 ? Color(1.0, random_double(0.0, 1.0), 0.0) : Color(0.0, 0.0, 0.0);
+				}
+				if (j >= 256 && j <= 512) {
+					c = random_double(0.0, 1.0) >= 0.5 ? Color(random_double(0.0, 1.0), 0.7, 0.25) : Color(0.0, 0.0, 0.0);
+				}
+				if (j >= 512 && j <= 768) {
+					c = random_double(0.0, 1.0) >= 0.5 ? Color(random_double(0.0, 1.0), 1.0, random_double(0.0, 1.0)) : Color(0.0, 0.0, 0.0);
+				}
+				if (j >= 768 && j <= 1024) {
+					c = random_double(0.0, 1.0) >= 0.5 ? Color(random_double(0.0, 1.0), random_double(0.0, 1.0), random_double(0.0, 1.0)) : Color(0.0, 0.0, 0.0);
+				}
+			}
+
+			if (i >= 0 && i <= 192) {
+				if (j >= 0 && j <= 255) {
+					c = random_double(0.0, 1.0) >= 0.5 ? Color(1.0, 0.0, 1.0) : Color(0.0, 0.0, 0.0);
+				}
+				if (j >= 256 && j <= 512) {
+					c = random_double(0.0, 1.0) >= 0.5 ? Color(random_double(0.0, 1.0), random_double(0.0, 1.0), 0.0) : Color(0.0, 0.0, 0.0);
+				}
+				if (j >= 512 && j <= 768) {
+					c = random_double(0.0, 1.0) >= 0.5 ? Color(0.0, random_double(0.0, 1.0), 1.0) : Color(0.0, 0.0, 0.0);
+				}
+				if (j >= 768 && j <= 1024) {
+					c = random_double(0.0, 1.0) >= 0.5 ? Color(1.0, 0.0, random_double(0.0, 1.0)) : Color(0.0, 0.0, 0.0);
+				}
+			}
+
+			WriteColor(myfile, c);
+		}
+	}
+
+	myfile.close();
+}
+
+int main() {
+
+	GenerateFirstImage(1024, 1024);
+	GenerateSecondImage(1024, 768);
 
 	return 0;
 }
--- L2_hw/vector3.h
+++ L2_hw/vector3.h
@@ -1,7 +1,6 @@
 #ifndef VECTOR3_H
 #define VECTOR3_H
 
-#include <cmath>
 #include <iostream>
  
 class Vector3 {
